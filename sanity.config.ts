5c5016cdda58631716ffbadc5187540db07fc33d
aosmann <76514582+aosmann@users.noreply.github.com>
2025-03-12 14:19:00 +0000

    h

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,25 +1,32 @@
 import { defineConfig } from 'sanity';
 import { deskTool } from 'sanity/desk';
 import { visionTool } from '@sanity/vision';
+//import {googleMapsInput} from '@sanity/google-maps-input'
 import { schemaTypes } from './schemas';
 import Logo from './components/Logo';
 import StudioNavbar from './components/StudioNavbar';
 
 const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!;
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!;
+const apiKey = process.env.GOOGLE_MAPS_API_KEY!;
+
+import { googleMapsInput } from '@sanity/google-maps-input';
 
 export default defineConfig({
   name: 'default',
   title: 'Ingigante Property Management',
-  basePath: '/admin', // ← instead of '/studio'
+  basePath: '/studio',
 
   projectId,
   dataset,
 
   plugins: [
+    googleMapsInput({
+      apiKey: 'AIzaSyCNhsU6R9HsP40Xu9QTwvWKCeWAZdpSRfM',
+    }),
     deskTool(),
     visionTool(),
-    // ✅ No need to include googleMapsInput here
+    //googleMapsInput(),
   ],
 
   schema: {
