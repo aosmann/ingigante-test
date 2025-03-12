03fa84056150afdcdaf21dd54e71849340f9b174
aosmann <76514582+aosmann@users.noreply.github.com>
2025-03-12 14:06:26 +0000

    s

--- pages/studio/[[...index]].tsx
+++ pages/studio/[[...index]].tsx
@@ -1,6 +1,6 @@
 import { NextStudio } from 'next-sanity/studio'
-import config from '../sanity.config'
+import config from '../../sanity.config'
 
-export default function AdminStudio() {
+export default function IndexStudio() {
   return <NextStudio config={config} />
 }
