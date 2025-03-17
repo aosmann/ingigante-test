777cee248e8c15299f6f0016dddafc6b9e9ec3b6
aosmann <76514582+aosmann@users.noreply.github.com>
2025-03-17 06:50:16 +0000

    n

--- components/Newsletter.tsx
+++ components/Newsletter.tsx
@@ -1,6 +1,4 @@
-import React from "react";
-import { useState } from "react";
-import { client_with_token } from "../lib/sanity.client";
+import React, { useState } from "react";
 
 const Newsletter = () => {
   const [state, setState] = useState(0);
@@ -8,12 +6,11 @@ const Newsletter = () => {
   const [email, setEmail] = useState("");
   // 0 - initial , 1 - loading, 2 - success, 3 - error
 
-  const subscribe = async (e) => {
+  const subscribe = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    const email = e.target[0].value;
     setState(1);
     setErrorMsg("");
-    console.log(e.target[0].value);
+
     try {
       const res = await fetch("/api/newsletter", {
         method: "POST",
@@ -25,25 +22,22 @@ const Newsletter = () => {
         throw data.error;
       }
       setState(2);
-    } catch (e) {
-      setErrorMsg(e);
+    } catch (err: any) {
+      setErrorMsg(err?.message || "Something went wrong");
       setState(3);
     }
   };
+
   return (
     <section className="text-primary bg-[#F4F4F4] px-4 py-10 flex flex-col justify-center items-center">
       <div className="max-w-7xl flex justify-between w-full md:items-center flex-col md:flex-row space-y-5 px-4">
         <div>
           <h1 className="text-[33px]">Subscribe to newsletter</h1>
-          <p className="text-[16px] md:text-[20px]">
-            Get the latest news and offers
-          </p>
+          <p className="text-[16px] md:text-[20px]">Get the latest news and offers</p>
         </div>
         <div>
-          {state == 2 ? (
-            <p className="font-medium mt-4 text-xl text-green-800">
-              Thanks for subscribing!
-            </p>
+          {state === 2 ? (
+            <p className="font-medium mt-4 text-xl text-green-800">Thanks for subscribing!</p>
           ) : (
             <form
               className="md:flex md:flex-row md:items-center md:space-x-2 space-y-3 md:space-y-0"
@@ -63,10 +57,8 @@ const Newsletter = () => {
               >
                 Subscribe
               </button>
-              {state === 3 ? (
+              {state === 3 && (
                 <p className="text-red-500 mt-3">{errorMsg}</p>
-              ) : (
-                ""
               )}
             </form>
           )}
