8da87f29087466f43ea841742ef8b8cf08b52bf8
aosmann <76514582+aosmann@users.noreply.github.com>
2025-03-17 08:03:50 +0000

    a

--- lib/querySearch.ts
+++ lib/querySearch.ts
@@ -1,11 +1,14 @@
 import { client } from "./sanity.client";
 
-export async function querySearch(searchTerm) {
+export async function querySearch(searchTerm: string) {
   const query =
-    searchTerm == ""
+    searchTerm === ""
       ? `*[_type == "properties"]`
-      : `*[_type == "properties" && title match "${searchTerm}*" || city match "${searchTerm}*"]`;
-  const response = await client.fetch(query);
+      : `*[_type == "properties" && (title match $term || city match $term)]`;
+
+  const params = searchTerm ? { term: `${searchTerm}*` } : {};
+
+  const response = await client.fetch(query, params);
 
   return response;
 }
