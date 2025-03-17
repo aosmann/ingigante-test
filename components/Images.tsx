84c42127da7fbe6cfa33fa8479e9ffa0cf255f48
aosmann <76514582+aosmann@users.noreply.github.com>
2025-03-17 06:41:10 +0000

    s

--- components/Images.tsx
+++ components/Images.tsx
@@ -1,6 +1,11 @@
 import urlFor from '../lib/urlFor';
 
-const Images = ({ identifier, image }) => {
+interface ImagesProps {
+  identifier: string;
+  image: any; // or type this better if you know it's SanityImageAsset
+}
+
+const Images = ({ identifier, image }: ImagesProps) => {
   return (
     <div className={identifier === 'main-image' ? 'main-image' : 'image'}>
       <img src={urlFor(image).auto('format').url()} alt="property image" />
